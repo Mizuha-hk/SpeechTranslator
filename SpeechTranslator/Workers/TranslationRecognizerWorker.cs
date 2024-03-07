d09840e54fc7cae384df0508a590545fd66b8347
Miz ha <hk.haru0803@gmail.com>
2024-03-07 12:12:36 +0900

    [add] #4 未実装Workerの追加

---
+++ SpeechTranslator/Workers/TranslationRecognizerWorker.cs
@@ -0,0 +1,39 @@
+﻿namespace SpeechTranslator.Worker;
+
+public class TranslationRecognizerWorker: TranslationRecognizerWorkerBase
+{
+    public override void OnRecognizing(TranslationRecognitionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnRecognized(TranslationRecognitionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnCanceled(TranslationRecognitionCanceledEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnSpeechStartDetected(RecognitionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnSpeechEndDetected(RecognitionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnSessionStarted(SessionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void OnSessionStopped(SessionEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+}
